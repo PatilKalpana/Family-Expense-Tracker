a292ff775ee28facf7363a5c483185ea819f58a9
PatilKalpana <125743066+PatilKalpana@users.noreply.github.com>
2023-02-18 19:11:28 +0530

    Add files via upload

---
+++ Controllers/FamilyExpenseController.cs
@@ -0,0 +1,78 @@
+﻿using FamilyExpenseTracker.MVC1.Models.FamilyExpense;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyExpenseTracker.MVC1.Controllers
+{
+    public class FamilyExpenseController : Controller
+    {
+        // GET: FamilyExpense
+        public ActionResult Index()
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            List<FamilyExpense> familyExpenses = familyExpenseRepository.GetFamilyExpenses();
+            return View(familyExpenses);
+        }
+
+        public ActionResult AddFamilyExpense()
+        {
+            FamilyExpense familyExpense = new FamilyExpense();
+            return View(familyExpense);
+        }
+
+        [HttpPost]
+        public ActionResult AddFamilyExpense(FamilyExpense familyExpense)
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            //List<string> familyMemberNames = familyExpenseRepository.GetNames();
+            int insertStatus = familyExpenseRepository.AddFamilyExpense(familyExpense);
+            if (insertStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult EditFamilyExpense(int id)
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            FamilyExpense familyExpenseResult = familyExpenseRepository.GetFamilyExpense(id);
+            return View(familyExpenseResult);
+        }
+
+        [HttpPost]
+        public ActionResult EditFamilyExpense(FamilyExpense familyExpense)
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            int editStatus = familyExpenseRepository.EditFamilyExpense(familyExpense);
+            if (editStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult DeleteFamilyExpense(int id)
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            FamilyExpense familyExpenseResult = familyExpenseRepository.GetFamilyExpense(id);
+            return View(familyExpenseResult);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFamilyExpense(FamilyExpense familyExpense)
+        {
+            FamilyExpenseRepository familyExpenseRepository = new FamilyExpenseRepository();
+            int deleteStatus = familyExpenseRepository.DeleteFamilyExpense(Convert.ToInt32(familyExpense.ExpenseId));
+            if (deleteStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/FamilyMemberController.cs
@@ -0,0 +1,76 @@
+﻿using FamilyExpenseTracker.MVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FamilyExpenseTracker.MVC1.Controllers
+{
+    public class FamilyMemberController : Controller
+    {
+        // GET: FamilyMember
+        public ActionResult Index()
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            List<FamilyMember> familyMembers = familyMemberRepository.GetAllFamilyMembers();
+            return View(familyMembers);
+        }
+
+        public ActionResult AddFamilyMember()
+        {
+            FamilyMember familyMember = new FamilyMember();
+            return View(familyMember);
+        }
+
+        [HttpPost]
+        public ActionResult AddFamilyMember(FamilyMember familyMember)
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            int insertStatus = familyMemberRepository.AddFamilyMember(familyMember);
+            if (insertStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult EditFamilyMember(int id)
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            FamilyMember familyMember = familyMemberRepository.GetFamilyMember(id);
+            return View(familyMember);
+        }
+
+        [HttpPost]
+        public ActionResult EditFamilyMember(FamilyMember familyMember)
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            int editStatus = familyMemberRepository.EditFamilyMember(familyMember);
+            if (editStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult DeleteFamilyMember(int id)
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            FamilyMember familyMember = familyMemberRepository.GetFamilyMember(id);
+            return View(familyMember);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFamilyMember(FamilyMember familyMember)
+        {
+            FamilyMemberRepository familyMemberRepository = new FamilyMemberRepository();
+            int deleteStatus = familyMemberRepository.DeleteFamilyMember(Convert.ToInt32(familyMember.FamilyMemberId));
+            if (deleteStatus > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
