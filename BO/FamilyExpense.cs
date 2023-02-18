afe32baa24af407455060fade4668aed858cdb49
PatilKalpana <125743066+PatilKalpana@users.noreply.github.com>
2023-02-18 18:40:57 +0530

    Add files via upload

---
+++ BO/FamilyExpense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class FamilyExpense
+    {
+        public int ExpenseId { get; set; }
+        public int FamilyMemberId { get; set; }
+        public string Name { get; set; }
+        public string Purpose { get; set; }
+        public int Amount { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ BO/FamilyMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public class FamilyMember
+    {
+        public int? FamilyMemberId { get; set; }
+        public string Name { get; set; }
+        public long Cell { get; set; }
+        public string Work { get; set; }
+        public int? Income { get; set; }
+    }
+}
